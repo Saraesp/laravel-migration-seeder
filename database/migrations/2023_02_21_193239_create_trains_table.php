944b58928c2bfc46f5f50b3074a97a930b642aae
Saraesp <saraesposito145@gmail.com>
2023-02-23 17:42:09 +0100

    creazione columns

--- database/migrations/2023_02_21_193239_create_trains_table.php
+++ database/migrations/2023_02_21_193239_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 7);
+            $table->smallInteger('num_carroze');
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('cancellato')->default(1);
             $table->timestamps();
         });
     }
